50590310f23f016423d6b40799e0a8d8172562c0
Madeleine Hartland-Dykes <146767175+hd2802@users.noreply.github.com>
2024-02-15 09:07:55 +0000

    fix: jest.js config

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-
 import Router from './Router.js';
+
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Router/>
-    </div>
+    </BrowserRouter>
   );
 }
 
