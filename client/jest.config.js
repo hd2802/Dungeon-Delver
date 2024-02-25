0ac1834c5ef4f13de57fc294d3a21b9af4b5bc92
Madeleine Hartland-Dykes <146767175+hd2802@users.noreply.github.com>
2024-02-25 18:27:06 +0000

    reset

--- client/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    "moduleNameMapper": {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "client/config/CSSStub.js",
-        "\\.(scss|sass|css)$": "identity-obj-proxy"
-      },
-};
