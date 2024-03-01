8704cee5b4f3f3d93325d73c063645ad60eb4017
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2024-03-01 15:16:30 +0000

    Stylises and edits app/trial-campaign

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,10 +6,13 @@ export default function Footer() {
             <footer className="footer">
                 <div className="footer-container">
                     <p className="footer-info">
-                    Dungeon Delver is unofficial Fan Content permitted under the Fan Content Policy. It is not approved/endorsed by Wizards of the Coast. Portions of the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC
+                    Dungeon Delver is unofficial Fan Content permitted under the <a href="https://company.wizards.com/en/legal/fancontentpolicy">Fan Content Policy</a>. It is not approved/endorsed by Wizards of the Coast.
                     </p>
                     <p className="footer-info">
                     Dungeon Delver may use the trademarks and other intellectual property of Wizards of the Coast LLC, which is permitted under the Open Gaming Liscence (OGL).
+                    </p>
+                    <p className="footer-info">
+                    Unless otherwise stated, the content of this page is licensed under <a href="https://creativecommons.org/licenses/by-sa/3.0/"> Creative Commons Attribution-ShareAlike 3.0 License </a>
                     </p>
                 </div>
         </footer>
