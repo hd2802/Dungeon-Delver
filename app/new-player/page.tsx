8a74c474f0d28a3eff3d33b88cbac1a7a87706b0
Madeleine Hartland-Dykes <146767175+hd2802@users.noreply.github.com>
2024-03-13 16:38:49 +0000

    Fixes failed tests

--- app/new-player/page.tsx
+++ app/new-player/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import "../styles/Dash.css";
 
-export default function tutorial() {
+export default function NewPlayer() {
     return (
         <div className="outer-div-dash">
             <div className="header-div-dash">
@@ -10,22 +10,19 @@ export default function tutorial() {
             
             <div className="dash-section">
                 <h2> Interactive Features</h2>
-                <Link className="dash-footer" id="trial" href="/trial-campaign">Trial Campaign</Link>
-                <Link className="dash-footer" href="/character-builder">Character Creation Wizard</Link>
+                    <Link className="dash-footer" id="trial" href="/trial-campaign">Trial Campaign</Link>
+                    <Link className="dash-footer" href="/character-builder">Character Creation Wizard</Link>
             </div>
 
             <div className="dash-section">
-<<<<<<< HEAD
                 <h2>Information</h2>
-                <Link className="dash-footer" id="combat" href="/new-player/combat">Combat</Link>
-                <Link className="dash-footer" id="ability" href="/new-player/abilities">Ability Scores</Link>
-=======
+                    <Link className="dash-footer" id="combat" href="/new-player/combat">Combat</Link>
+                    <Link className="dash-footer" id="ability" href="/new-player/abilities">Ability Scores</Link>
                 <h2>Information</h2> 
-                <Link id="combat" href="/new-player/combat">Combat</Link>
-                <Link id="ability" href="/new-player/abilities">Ability Scores</Link>
-                <Link id="classes" href="/new-player/classes">Classes</Link>
-                <Link id="races" href="/new-player/races">Races</Link>
->>>>>>> fce991bf1b75614c9800b59843ec5db2e73c8192
+                    <Link id="combat" href="/new-player/combat">Combat</Link>
+                    <Link id="ability" href="/new-player/abilities">Ability Scores</Link>
+                    <Link id="classes" href="/new-player/classes">Classes</Link>
+                    <Link id="races" href="/new-player/races">Races</Link>
             </div>
         </div>
     )
